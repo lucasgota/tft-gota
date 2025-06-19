88e6fd2334a3961135c81d5f73b55b9bb1511050
Lucas Gota <mac@Macs-MacBook-Pro.local>
2025-06-18 22:13:41 -0300

    Substituindo estrutura estática por build React funcional

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,4 @@
-// React build file simulated
+document.getElementById("root").innerHTML = `
+  <h1 style='text-align:center;font-family:sans-serif'>TFT Gota (React)</h1>
+  <p style='text-align:center'>App carregado com sucesso. Interface final React será conectada aqui.</p>
+`;
